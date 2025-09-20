03871699519fc5f7d5ab08bb28c6ed6003902725
MoOfficial0000 <170979136+MoOfficial0000@users.noreply.github.com>
2025-09-20 22:35:52 +0100

    added buffed and changed some

--- ballsdex/packages/battle/cog.py
+++ ballsdex/packages/battle/cog.py
@@ -39,8 +39,8 @@ if TYPE_CHECKING:
 log = logging.getLogger("ballsdex.packages.battle")
 
 battles = []
-highevent = ("Testers","Birthday Ball","Eid al-Adha 1445","Realm","Event Farmer","American","Dragon Ball","Aerial Tramway")
-lowevent = ("Lunar New Year 2025","Winter 2024","Summer","Spring Basket 2025","Dark Mist 2024","Goku Day 2025","Birthday 2025","Eid al-Adha 1446")
+highevent = ("Testers","Birthday Ball","Eid al-Adha 1445","Realm","Event Farmer","American","Dragon Ball","Aerial Tramway","Birthday 2025","Champion Edition Goku","Champion Edition Vegeta","International Cat Day 2025 (Larry)")
+lowevent = ("Lunar New Year 2025","Winter 2024","Summer","Spring Basket 2025","Dark Mist 2024","Goku Day 2025","Eid al-Adha 1446","Autumn 2025","International Cat Day 2025 (Rigby)")
 
 @dataclass
 class GuildBattle:
